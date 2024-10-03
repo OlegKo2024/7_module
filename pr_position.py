cc0e2d552b6a2815914b2d80ee59b8dbc20254ff
OlegKo2024 <4oki66@gmail.com>
2024-10-04 00:24:25 +0300

    Add files via upload

--- pr_position.py
+++ pr_position.py
@@ -11,9 +11,9 @@ def custom_write(file_name, strings):
     read_to_dict = {}
     for i in range(len(strings)):
         byte_position = file.tell()  # Получаем текущее положение курсора (начало строки)
-        line = file.readline()  # Считаем строку
-        key = (i + 1, byte_position)  # Сохраняем номер строки, положение курсора и строку в словаре
-        read_to_dict[key] = line.rstrip('\n')  # Убираем символ новой строки
+        line = file.readline()  # Считываем строку
+        key = (i + 1, byte_position)  # Сохраняем номер строки, положение курсора в переменной key
+        read_to_dict[key] = line.rstrip('\n')  # Записываем ключ и значение в словарь, убираем символ новой строки
     file.close()
     return read_to_dict
 
@@ -42,11 +42,11 @@ def custom_write(file_name, strings):
     read_file = file.readlines()                # Считываем файл построчно в список
     file.seek(0)                                # Переходим в начало файла после чтения
     read_to_dict = {}
-    for i in range(len(read_file)):
-        byte_position = file.tell()             # Получаем текущее положение курсора (начало строки)
-        line = file.readline()                  # Считаем строку
-        key = (i + 1, byte_position)            # Сохраняем номер строки, положение курсора и строку в словаре
-        read_to_dict[key] = line.rstrip('\n')   # Убираем символ новой строки
+    for i in range(len(read_file)):             # Используем считанный в список файл для задания кол-ва итераций
+        byte_position = file.tell()
+        line = file.readline()
+        key = (i + 1, byte_position)
+        read_to_dict[key] = line.rstrip('\n')
     file.close()
     return read_to_dict
 
